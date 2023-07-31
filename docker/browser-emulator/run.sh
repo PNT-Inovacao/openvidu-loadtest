35da88f5ee62f8c1703b9d709a8049709f0aa496
Vitor Diego <vitordiegossousa@gmail.com>
2023-07-31 18:36:16 -0300

    Fix: attach version 3.1.0 for visqol in Dockerfile

--- docker/browser-emulator/run.sh
+++ docker/browser-emulator/run.sh
@@ -4,7 +4,7 @@ if [[ -z "$1" ]]; then
     if [[ -z "$1" ]]; then
         echo "RELEASE_VERSION argument is required" 1>&2
     fi
-	echo "Example of use: ./run.sh 2.1.0" 1>&2
+    echo "Example of use: ./run.sh 2.1.0" 1>&2
     exit 1
 fi
 
